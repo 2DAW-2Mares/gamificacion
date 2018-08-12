cdc8aff107289cf83d86e557c1eee16d5d01f29d
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-08-12 09:05:01 +0200

    Asignando automáticamente al creador de cada juego.

--- common/models/juego.js
+++ common/models/juego.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function (Juego) {
-
+  Juego.beforeRemote('create', function (context, juego, next) {
+    context.args.data.creador = context.req.accessToken.userId;
+    next();
+  });
 };
