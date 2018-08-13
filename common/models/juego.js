6fd42986a4eefeb10cd9f36419576ccf1514c6ee
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-08-13 19:07:39 +0200

    Validando los modelos

--- common/models/juego.js
+++ common/models/juego.js
@@ -5,4 +5,7 @@ module.exports = function (Juego) {
     context.args.data.creador = context.req.accessToken.userId;
     next();
   });
+
+  //TODO En el caso de unificar todas las imágenes en un modelo, trasladar esta validación al nuevo modelo
+  Juego.validatesFormatOf('imagen', {with: '(http(s?):)|([/|.|\\w|\\s])*\\.(?:jpg|jpeg|gif|png|bmp|tiff|psd|svg)'});
 };
--- common/models/marcador.js
+++ common/models/marcador.js
@@ -2,4 +2,14 @@
 
 module.exports = function (Marcador) {
 
+  Marcador.validateAsync('puntos', puntosMenorIgualMaximo, {message: 'Los puntos superan el máximo para esta prueba'});
+
+  function puntosMenorIgualMaximo(err, done) {
+    let Prueba = Marcador.app.models.Prueba;
+    let self = this;
+    Prueba.findById(this.pruebaId, function (error, prueba) {
+      if (self.puntos > prueba.maximo) err();
+      done();
+    })
+  }
 };
