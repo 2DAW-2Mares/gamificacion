c16d0095d1b580b34b3bb498004a89f35be03160
Alberto Sierra <alberto.sierra@murciaeduca.es>
2018-10-08 09:42:58 +0200

    Método remoto getNombre en el modelo Juego

--- common/models/juego.js
+++ common/models/juego.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Juego) {
+    /**
+     * Devuelve el nombre de un Juego enviado como parámetro
+     * @param {Function(Error, string)} callback
+     */
+
+    Juego.prototype.getNombre = function(callback) {
+      var nombre = this.nombre;
+      // TODO
+      callback(null, nombre);
+    };
 
 };
