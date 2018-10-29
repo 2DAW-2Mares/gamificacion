3171fb9276a00377f535d08d0282fa84094500bc
Daniel Sanchez <d0xcodex0s@gmail.com>
2018-10-29 09:01:10 +0100

    remote metod grupales + acl

--- common/models/juego.js
+++ common/models/juego.js
@@ -1,15 +1,21 @@
 'use strict';
 
-module.exports = function(Juego) {
-    /**
-     * Devuelve el nombre de un Juego enviado como parámetro
-     * @param {Function(Error, string)} callback
-     */
+module.exports = function (Juego) {
+  /**
+   * Devuelve el nombre de un Juego enviado como parámetro
+   * @param {Function(Error, string)} callback
+   */
 
-    Juego.prototype.getNombre = function(callback) {
-      var nombre = this.nombre;
-      // TODO
-      callback(null, nombre);
-    };
-
+  Juego.prototype.getNombre = function (callback) {
+    var nombre = this.nombre;
+    // TODO
+    callback(null, nombre);
+  };
+  Juego.esgrupal = function (callback) {
+    var juegos;
+    Juego.find({ where: { grupal: true } }, function (err, juegos) {
+      callback(null, juegos);
+    });
+  };
 };
+
